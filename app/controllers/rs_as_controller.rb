519bdf63b89ab21643a588bfb30e6577253ed6d0
Vladislav <vladdislav.georgiev@gmail.com>
2017-12-19 18:16:34 +0200

    removing dummy code

--- app/controllers/rs_as_controller.rb
+++ app/controllers/rs_as_controller.rb
@@ -28,11 +28,6 @@ class RsAsController < ApplicationController
     end
     [n,e,d]
   end
-  # GET /rsas
-  # GET /rsas.json
-  def index
-    @rsas = Rsa.all
-  end
 
   # GET /rsas/1
   # GET /rsas/1.json
@@ -58,50 +53,6 @@ class RsAsController < ApplicationController
         if rsa.save
           format.json {render json: {'id' => rsa.id}}
         end
-    end
-  end
-
-  # GET /rsas/1/edit
-  def edit
-  end
-
-  # POST /rsas
-  # POST /rsas.json
-  def create
-    @rsa = Rsa.new(rsa_params)
-
-    respond_to do |format|
-      if @rsa.save
-        format.html { redirect_to @rsa, notice: 'Rsa was successfully created.' }
-        format.json { render :show, status: :created, location: @rsa }
-      else
-        format.html { render :new }
-        format.json { render json: @rsa.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /rsas/1
-  # PATCH/PUT /rsas/1.json
-  def update
-    respond_to do |format|
-      if @rsa.update(rsa_params)
-        format.html { redirect_to @rsa, notice: 'Rsa was successfully updated.' }
-        format.json { render :show, status: :ok, location: @rsa }
-      else
-        format.html { render :edit }
-        format.json { render json: @rsa.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /rsas/1
-  # DELETE /rsas/1.json
-  def destroy
-    @rsa.destroy
-    respond_to do |format|
-      format.html { redirect_to rsas_url, notice: 'Rsa was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
