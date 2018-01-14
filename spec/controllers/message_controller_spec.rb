f4ea94a2647d3ba2adca4a93c593a00aadbfa516
Vladislav <vladdislav.georgiev@gmail.com>
2018-01-14 22:58:52 +0200

    added small comment

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe MessagesController do
         expect(JSON.parse(response.body)).not_to be_empty
     end
     it "should encrypt and decrypt a message" do   
-       post :decrypt, params: {id: 1, message: "15841:5339:10931:10931:4625:9048:19701:5689:5339:10566:5339:9048:15591:783:19969:5945:17032"}, as: :json
+       post :decrypt, params: {id: 1, message: "15841:5339:10931:10931:4625:9048:19701:5689:5339:10566:5339:9048:15591:783:19969:5945:17032"}, as: :json ## encrypted "Hello there guys!" with the keys for id 1
        expect(JSON.parse(response.body)["message"]).to eq "Hello there guys!"
       
     end
